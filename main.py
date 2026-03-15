f0aaa5f0dde5faff5667e547ff56f68b5ed1cd53
Rechner Fox <659028+rechner@users.noreply.github.com>
2026-03-14 23:35:14 -0700

    Make the webhook parsing more forgiving

--- main.py
+++ main.py
@@ -7,13 +7,28 @@ apobj.add(config)
 
 
 def main(request):
+    body_text = ""
     webhook_body = request.json
-    body_text = f'*{webhook_body["attachments"][0]["title"]}*\n'
-    body_text += f"{webhook_body['sections'][0]['activitySubtitle']}\n"
-    for field in webhook_body["attachments"][0]["fields"]:
-        body_text += f"{field['title']}: `{field['value']}`\n"
-    apobj.notify(
-        body=body_text,
-        title=f"*{webhook_body['text']}*",
-    )
+    attachments = webhook_body.get("attachments")
+    if attachments:
+        first_attachment = attachments[0]
+
+        title = first_attachment.get("title")
+        if title:
+            body_text = f'*{webhook_body["attachments"][0]["title"]}*\n'
+        activitySubtitle = first_attachment.get("activitySubtitle")
+        if activitySubtitle:
+            body_text += f"{webhook_body['sections'][0]['activitySubtitle']}\n"
+
+        for field in first_attachment.get("fields", []):
+            body_text += f"{field['title']}: `{field['value']}`\n"
+
+    print(f"Notify: {webhook_body['text']} - {body_text}")
+    body_text += f"{webhook_body['text']}\n"
+
+    apobj.notify(body=body_text)
+    # apobj.notify(
+    #    body=body_text,
+    #    title=f"*{webhook_body['text']}*",
+    # )
     return "OK"
